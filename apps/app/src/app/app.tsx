76683b975539475df9254d9fcfdb683f1ec2d33d
jgonzalez13 <jgonzalez13@gmail.com>
2022-08-09 11:17:37 -0500

    remove api-interfaces lib

--- apps/app/src/app/app.tsx
+++ apps/app/src/app/app.tsx
@@ -1,28 +1,12 @@
-import React, { useEffect, useState } from 'react';
-import { Message } from '@sdjs-02/api-interfaces';
-
-export const App = () => {
-  const [m, setMessage] = useState<Message>({ message: '' });
-
-  useEffect(() => {
-    fetch('/api')
-      .then((r) => r.json())
-      .then(setMessage);
-  }, []);
-
-  return (
-    <>
-      <div style={{ textAlign: 'center' }}>
-        <h1>Welcome to app!</h1>
-        <img
-          width="450"
-          src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
-          alt="Nx - Smart, Fast and Extensible Build System"
-        />
-      </div>
-      <div>{m.message}</div>
-    </>
-  );
-};
+export const App = () => (
+  <div style={{ textAlign: 'center' }}>
+    <h1>Welcome to app!</h1>
+    <img
+      width="450"
+      src="https://raw.githubusercontent.com/nrwl/nx/master/images/nx-logo.png"
+      alt="Nx - Smart, Fast and Extensible Build System"
+    />
+  </div>
+)
 
 export default App;
