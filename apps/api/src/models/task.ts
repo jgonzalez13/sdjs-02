ae593eaa481e372e438a93cc027464058633c4eb
jgonzalez13 <jgonzalez13@gmail.com>
2022-08-11 15:40:29 -0500

    moongose schema and formik validations

--- apps/api/src/models/task.ts
+++ apps/api/src/models/task.ts
@@ -7,10 +7,14 @@ export const TaskSchema = new mongoose.Schema({
   title: {
     type: String,
     required: true,
+    minLength: 10,
+    maxLength: 120,
   },
   description: {
     type: String,
     required: true,
+    minLength: 100,
+    maxLength: 1000,
   },
   status: {
     type: String,
--- apps/app/src/components/taskForm/taskFormSchema.ts
+++ apps/app/src/components/taskForm/taskFormSchema.ts
@@ -1,9 +1,9 @@
 import * as Yup from 'yup';
 
 const taskFormSchema = Yup.object({
-  title: Yup.string().required(),
+  title: Yup.string().required().min(10).max(120),
 
-  description: Yup.string().required(),
+  description: Yup.string().required().min(100).max(1000),
 });
 
 export default taskFormSchema;
